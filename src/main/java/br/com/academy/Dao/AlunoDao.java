3174dd3bcacc97ea85b805c0097f5f75021e8835
Decston <alissonpatriciodefreitas@gmail.com>
2020-11-24 16:53:02 -0300

    Funções de pesquisar por nome e por status concluidas.

--- src/main/java/br/com/academy/Dao/AlunoDao.java
+++ src/main/java/br/com/academy/Dao/AlunoDao.java
@@ -1,9 +1,26 @@
 package br.com.academy.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.academy.model.Aluno;
 
 public interface AlunoDao extends JpaRepository<Aluno, Integer> {
-
+	
+	@Query("select a from Aluno a where a.status = 'ATIVO'")
+	public List<Aluno> findByStatusAtivos();
+	
+	@Query("select a from Aluno a where a.status = 'INATIVO'")
+	public List<Aluno> findByStatusInativos();
+	
+	@Query("select a from Aluno a where a.status = 'CANCELADO'")
+	public List<Aluno> findByStatusCancelado();
+	
+	@Query("select a from Aluno a where a.status = 'TRANCADO'")
+	public List<Aluno> findByStatusTrancado();
+	
+	public List<Aluno> findByNomeContainingIgnoreCase(String nome); 
+	
 }
--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -1,5 +1,7 @@
 package br.com.academy.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.academy.Dao.AlunoDao;
@@ -24,6 +27,14 @@ public class AlunoController {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Aluno/formAluno");
 		mv.addObject("aluno", new Aluno());
+		
+		return mv;
+	}
+	
+	@GetMapping("/pesquisarAlunos")
+	public ModelAndView searchAlunos() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/filtroAlunos");
 		
 		return mv;
 	}
@@ -46,6 +57,7 @@ public class AlunoController {
 	public ModelAndView listagemAlunos() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Aluno/listAlunos");
+		mv.addObject("aluno", new Aluno());
 		mv.addObject("alunosList", alunorepositorio.findAll());
 		return mv;
 	}
@@ -72,5 +84,58 @@ public class AlunoController {
 		alunorepositorio.deleteById(id);
 		return "redirect:/alunos-adicionados";
 	}
+	
+	@GetMapping("filtro-alunos")
+	public ModelAndView filtroAlunos() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/filtroAlunos");
+		return mv;
+	}
+	
+	@GetMapping("alunos-ativos")
+	public ModelAndView listaAlunosAtivos() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/alunos-ativos");
+		mv.addObject("alunosAtivos", alunorepositorio.findByStatusAtivos());
+		return mv;
+	}
+	
+	@GetMapping("alunos-inativos")
+	public ModelAndView listaAlunosInativos() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/alunos-inativos");
+		mv.addObject("alunosInativos", alunorepositorio.findByStatusInativos());
+		return mv;
+	}
+	
+	@GetMapping("alunos-cancelados")
+	public ModelAndView listaAlunosCancelados() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/alunos-cancelados");
+		mv.addObject("alunosCancelados", alunorepositorio.findByStatusCancelado());
+		return mv;
+	}
+	
+	@GetMapping("alunos-trancados")
+	public ModelAndView listaAlunosTrancados() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Aluno/alunos-trancados");
+		mv.addObject("alunosTrancados", alunorepositorio.findByStatusTrancado());
+		return mv;
+	}
+	
+	@PostMapping("pesquisar-aluno")
+	public ModelAndView pesquisarAluno(@RequestParam(required = false) String nome) {
+		ModelAndView mv = new ModelAndView();
+		List<Aluno> listaAlunos;
+		if(nome == null || nome.trim().isEmpty()) {
+			listaAlunos = alunorepositorio.findAll();
+		}else {
+			listaAlunos = alunorepositorio.findByNomeContainingIgnoreCase(nome);
+		}
+		mv.addObject("ListaDeAlunos", listaAlunos);
+		mv.setViewName("Aluno/pesquisa-resultado");
+		return mv;
+	}	
 	
 }
